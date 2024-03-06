75ed78e68508333e54af2ef4b0b776c089c4bfe3
Lim-ji-hun <86521741+Lim-ji-hun@users.noreply.github.com>
2024-03-06 16:55:19 +0900

    day3-1
    재업

--- QuizNestedif.java
+++ QuizNestedif.java
@@ -18,31 +18,31 @@ public class QuizNestedif {
 		System.out.print("보유한 금액을 입력 > "); 
 		int money = scan.nextInt();
 		
-//		if(money > 0) {
-//			
-//			if(money < 5000 ) {
-//				if(money < 3000) {
-//					System.out.println("구매 가능한 아이템 :일반 검, 일반 방패, 물약"); 
-//				}else {
-//					System.out.println("구매 가능한 아이템 : 쇠 검, 쇠 방패, 에너지 드링크, 일반 검, 일반 방패, 물약"); 
-//				}
-//				
-//			} else {
-//				System.out.println("구매 가능한 아이템 : 아이템 팩키지, 쇠 검, 쇠 방패, 에너지 드링크, 일반 검, 일반 방패, 물약"); 
-//			}
-//
-//		} else {
-//			System.out.println("보유 금액이 없습니다"); 
-//		}
+		if(money > 0) {
+			
+			if(money < 5000 ) {
+				if(money < 3000) {
+					System.out.println("구매 가능한 아이템 :일반 검, 일반 방패, 물약"); 
+				}else {
+					System.out.println("구매 가능한 아이템 : 쇠 검, 쇠 방패, 에너지 드링크, 일반 검, 일반 방패, 물약"); 
+				}
+				
+			} else {
+				System.out.println("구매 가능한 아이템 : 아이템 팩키지, 쇠 검, 쇠 방패, 에너지 드링크, 일반 검, 일반 방패, 물약"); 
+			}
+
+		} else {
+			System.out.println("보유 금액이 없습니다"); 
+		}
 		
 		
 		if(money > 0) {
 			System.out.println("구매 가능 아이템");
-			System.out.println("일반 검, 일반 방패, 물약");
+			System.out.println("1 - 일반 검, 일반 방패, 물약");
 			if(money >= 3000) {
-				System.out.println("쇠 검, 쇠 방패, 에너지 드링크");
+				System.out.println("2 - 쇠 검, 쇠 방패, 에너지 드링크");
 				if(money >= 5000) {
-					System.out.println("아이템 팩키지");
+					System.out.println("3 - 아이템 팩키지");
 				}
 			}
 			
@@ -60,37 +60,37 @@ public class QuizNestedif {
 	    //   large       50kg 미만    cart-C
 	    //   large       50kg 이상    cart-D
 		
-//		System.out.print("제품의 크기(large or small) > "); 
-//		String size = scan.next();
-//		
-//		if(size.equals("large") || size.equals("small")) {
-//			System.out.print("제품의 무게(kg) > "); 
-//			double we = scan.nextDouble();
-//			
-//			if(size.equals("large")) {
-//				System.out.println("제품의 크기 : large");
-//				if(we >= 50) {
-//					System.out.println("제품의 무게 : 50kg 이상");
-//					System.out.println("cart-D");
-//				} else {
-//					System.out.println("제품의 무게 : 50kg 미만");
-//					System.out.println("cart-C");
-//				}
-//			} else {
-//				System.out.println("제품의 크기 : small"); 
-//				if(we >= 50) {
-//					System.out.println("제품의 무게 : 50kg 이상");
-//					System.out.println("cart-B");
-//				} else {
-//					System.out.println("제품의 무게 : 50kg 미만");
-//					System.out.println("cart-A");
-//				}
-//
-//			}
-//		
-//		} else {
-//			System.out.println("다시 입력하시오");
-//		}
+		System.out.print("제품의 크기(large or small) > "); 
+		String size = scan.next();
+		
+		if(size.equals("large") || size.equals("small")) {
+			System.out.print("제품의 무게(kg) > "); 
+			double we = scan.nextDouble();
+			
+			if(size.equals("large")) {
+				System.out.println("제품의 크기 : large");
+				if(we >= 50) {
+					System.out.println("제품의 무게 : 50kg 이상");
+					System.out.println("cart-D");
+				} else {
+					System.out.println("제품의 무게 : 50kg 미만");
+					System.out.println("cart-C");
+				}
+			} else {
+				System.out.println("제품의 크기 : small"); 
+				if(we >= 50) {
+					System.out.println("제품의 무게 : 50kg 이상");
+					System.out.println("cart-B");
+				} else {
+					System.out.println("제품의 무게 : 50kg 미만");
+					System.out.println("cart-A");
+				}
+
+			}
+		
+		} else {
+			System.out.println("다시 입력하시오");
+		}
 
 				
 	}		
